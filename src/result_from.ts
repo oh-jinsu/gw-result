81c11c9f704c143d519e41d8bf9547a34ef76bf6
oh-jinsu <ohjinsu98@icloud.com>
2026-04-10 17:07:04 +0900

    Use function overloads

--- src/result_from.ts
+++ src/result_from.ts
@@ -1,27 +1,27 @@
 import { err, ok, type Result } from "./result";
 
-type AnyFn = (...args: any[]) => any;
-
-type ReturnResult<Fn extends AnyFn> =
-  ReturnType<Fn> extends Promise<infer R>
-    ? Promise<Result<R>>
-    : Result<ReturnType<Fn>>;
-
-export function resultFrom<Fn extends AnyFn, TParams extends Parameters<Fn>>(
-  Fn: Fn,
+export function resultFrom<TParams extends any[], R>(
+  Fn: (...args: TParams) => Promise<R>,
   ...args: TParams
-): ReturnResult<Fn> {
+): Promise<Result<R>>;
+
+export function resultFrom<TParams extends any[], R>(
+  Fn: (...args: TParams) => R,
+  ...args: TParams
+): Result<R>;
+
+export function resultFrom(fn: any, ...args: any[]): any {
   try {
-    const result = Fn(...args);
+    const result = fn(...args);
 
     if (result instanceof Promise) {
       const promise = result.then((res) => ok(res)).catch((e) => err(e));
 
-      return promise as ReturnResult<Fn>;
+      return promise as any;
     }
 
-    return ok(result) as ReturnResult<Fn>;
+    return ok(result) as any;
   } catch (e) {
-    return err(e) as ReturnResult<Fn>;
+    return err(e) as any;
   }
 }
